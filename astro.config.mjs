eeb02eed582ac1289325a3e650bb9e3a49148a46
Max Santana <maxsantana92ok@gmail.com>
2025-08-31 17:50:13 -0300

    Added node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
+import node from '@astrojs/node'
 
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://MaxSantana92.github.io/portfolio',
   //base: '/portfolio',
   output: 'static',
+  adapter: node({
+    mode: 'standalone',
+  }),
 })
