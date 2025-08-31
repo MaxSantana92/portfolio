03bb882ef009f3b2313e9767f1aaff7ffc5e7032
Max Santana <maxsantana92ok@gmail.com>
2025-08-31 18:36:39 -0300

    added base name repository

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from '@astrojs/tailwind'
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://MaxSantana92.github.io/portfolio',
-  //base: '/portfolio',
+  base: '/portfolio',
   output: 'static',
 })
